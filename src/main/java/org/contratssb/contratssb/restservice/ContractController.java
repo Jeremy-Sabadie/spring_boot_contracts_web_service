3ddb6b586a2a898f9537bdd96393bbde4bc76766
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2024-01-25 18:44:05 +0100

    Put and delete road mapping pepared.

--- src/main/java/org/contratssb/contratssb/restservice/ContractController.java
+++ src/main/java/org/contratssb/contratssb/restservice/ContractController.java
@@ -2,10 +2,7 @@ package org.contratssb.contratssb.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.contratssb.contratssb.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ContractController {
@@ -23,5 +20,18 @@ public class ContractController {
     @GetMapping("/contract/{id}")
     public String contract(@PathVariable int id){
         return String.format("Contrat numéro : %d", id);
+    }
+    @PostMapping("/contract")
+    public String createContract(@RequestBody Contract contract) {
+        // Logique pour créer un contrat avec les données passées dans le body de la requête.
+        return "Contrat créé avec succès";}
+    @PutMapping("/contract/{id}")
+    public String updateContract(@PathVariable int id, @RequestBody /*dtoRequestContract dto*/) {
+        // Appel de la méthode de la bll.
+        return "Contrat mis à jour avec succès";}
+    @DeleteMapping("/contract/{id}")
+    public String deleteContract(@PathVariable int id) {
+        //Appel de la méthode delete de la bll
+        return "Contrat supprimé avec succès";
     }
 }
